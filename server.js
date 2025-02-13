6c09f735cef34d65921ee3e990eee433790c0d80
queenscout <zachhowks@gmail.com>
2025-02-13 11:01:41 +0800

    Added default route for homepage

--- server.js
+++ server.js
@@ -11,5 +11,9 @@ app.use(cors());
 connectDB();
 app.use("/", routes);
 
+app.get("/", (req, res) => {
+    res.send("Bestocks Backend is Running!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
